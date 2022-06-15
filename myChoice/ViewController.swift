321aeeee2a32ade1b0916ecb7489b86f44146911
Shirel <shirelquintanilla35@gmail.com>
2022-06-14 22:09:05 -0600

    initial screen v. 1

--- myChoice/ViewController.swift
+++ myChoice/ViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var location: UIButton!
+    
+    @IBOutlet weak var yourLocation: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setPopupButton()
+    }
+    
+    func setPopupButton() {
+        let optionClosure = {(action : UIAction) in print(action.title)}
+        
+        location.menu = UIMenu(children :[
+        
+            UIAction(title : "Houston", state: .on, handler: optionClosure),
+            UIAction(title : "Dallas", handler: optionClosure),
+            UIAction(title : "Austin", handler: optionClosure),
+            UIAction(title : "El Paso", handler: optionClosure),
+            UIAction(title : "San Antonio", handler: optionClosure),
+        ])
+        
+        location.showsMenuAsPrimaryAction = true
+        location.changesSelectionAsPrimaryAction = true
+        
+        //self.yourLocation.text = optionClosure.children
+        
+      
     }
 
 
