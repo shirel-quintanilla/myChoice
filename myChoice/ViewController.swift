5fc62d45cd5d7fbf8e941ea0949ca6a776ce480c
victoria <happyfunnyvicky@gmail.com>
2022-06-15 00:12:54 -0500

    jkjklkk

--- myChoice/ViewController.swift
+++ myChoice/ViewController.swift
@@ -8,28 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var location: UIButton!
     
-    @IBOutlet weak var yourLocation: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        setPopupButton()
-    }
     
-    func setPopupButton() {
-        let optionClosure = {(action : UIAction) in print(action.title)}
-        
-        location.menu = UIMenu(children :[
-        
-            UIAction(title : "Houston", state: .on, handler: optionClosure),
-            UIAction(title : "Dallas", handler: optionClosure),
-            UIAction(title : "Austin", handler: optionClosure),
-            UIAction(title : "El Paso", handler: optionClosure),
-            UIAction(title : "San Antonio", handler: optionClosure),
-        ])
-        
-        location.showsMenuAsPrimaryAction = true
-        location.changesSelectionAsPrimaryAction = true
+
         
         //self.yourLocation.text = optionClosure.children
         
@@ -37,5 +20,6 @@ class ViewController: UIViewController {
     }
 
 
-}
 
+
+}
